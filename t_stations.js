01230e007f5b86a19d192634aa57b90b7549c621
Cortney Logan <logan.cort@gmail.com>
2021-06-16 21:45:58 -0400

    updating code from workshop

--- t_stations.js
+++ t_stations.js
@@ -1,1 +1,65 @@
+// STATE MACHINES
 
+// State Transition Diagram - map!
+//    Current state
+let currentStation = "Copley";
+
+//    Allowable transitions
+let allowableTransitions = {
+  "Hynes Convention Center": ["Copley"],
+  Copley: ["Arlington", "Hynes Convention Center", "Prudential"],
+  Prudential: ["Copley", "Symphony", "Northeastern"],
+  Symphony: ["Prudential", "Northeaster"],
+  Northeastern: ["Symphony"],
+  Arlington: ["Copley", "Boylston"],
+  Boylston: ["Park St", "Arlington", "Downtown Crossing"],
+  "Park St": ["Gov't Center", "Downtown Crossing", "Charles/MGH", "Boylston"],
+  "Gov't Center": ["Bowdoin", "Haymarket", "State", "Park St"],
+  Haymarket: ["Gov't Center"],
+
+  "Kendall/MIT": ["Charles/MGH"],
+  "Charles/MGH": ["Park St", "Kendall/MIT"],
+  "Downtown Crossing": [
+    "Park St",
+    "State",
+    "South Station",
+    "Boylston",
+    "Chinatown",
+  ],
+  "South Station": ["Downtown Crossing", "Broadway", "Courthouse", "Chinatown"],
+  Courthouse: ["South Station"],
+  Broadway: ["South Station"],
+
+  Bowdoin: ["Gov't Center"],
+  State: ["Gov't Center", "Aquarium", "Downtown Crossing", "Haymarket"],
+  Aquarium: ["State"],
+
+  Chinatown: ["Tufts Medical Center", "Downtown Crossing", "South Station"],
+  "Tufts Medical Center": ["Chinatown", "Back Bay", "Herald St"],
+  "Back Bay": ["Tufts Medical Center", "Mass. Ave"],
+  "Mass. Ave": ["Back Bay"],
+
+  "Herald St": ["Tufts Medical Center", "East Berkeley St"],
+  "East Berkeley St": ["Herald St", "Union Park St"],
+  "Union Park St": ["East Berkeley St"],
+};
+
+//    Move between states
+function transferStations(newStation) {
+  // receive the new location
+  // IF the transition is valid change it to a new location
+  if (allowableTransitions[currentStation].includes(newStation)) {
+    // update the current location to new location
+    currentStation = newStation;
+  }
+  // ELSE the transition is not valid, throw an error message
+  else {
+    console.log(`Sorry, we cannot go to ${newStation} from ${currentStation}`);
+  }
+}
+
+console.log(currentStation) // => copley
+transferStations("Prudential") // 
+console.log(currentStation) // => Prudential
+transferStations("Haymarket") // Sorry, we cannot....
+console.log(currentStation) // => Prudential
--- t_trianlines.js
+++ t_trianlines.js
@@ -1,11 +1,96 @@
 // -------------------- Starter Code -------------------- //
-//const that set up the interface with the terminal, allowing a user to interact with your program
-const readline = require("readline");
-const rl = readline.createInterface(process.stdin, process.stdout);
-
-//function establishing an ask function to await user input
-function ask(questionText) {
-  return new Promise((resolve, reject) => {
-    rl.question(questionText, resolve);
-  });
+// define class of trainlines
+class TrainLines {
+  constructor(color, stations, connectingLines) {
+    this.color = color;
+    this.stations = stations;
+    this.connectingLines = connectingLines;
+  }
 }
+
+// declares each of the train lines as an object of the TrainLines class
+let blueLine = new TrainLines(
+  "blue",
+  ["Bowdoin", "Gov't Center", "State", "Aquarium"],
+  ["green", "orange"]
+);
+
+let redLine = new TrainLines(
+  "red",
+  [
+    "Kendall/MIT",
+    "Charles/MGH",
+    "Park St",
+    "Downtown Crossing",
+    "South Station",
+    "Broadway",
+  ],
+  ["orange", "green", "grey"]
+);
+
+let orangeLine = new TrainLines(
+  "orange",
+  [
+    "Haymarket",
+    "State",
+    "Downtown Crossing",
+    "Chinatown",
+    "Tufts Medical Center",
+  ],
+  ["blue", "green", "red", "grey"]
+);
+
+let greenLine = new TrainLines(
+  "green",
+  [
+    "Haymarket",
+    "Gov't Center",
+    "Park St",
+    "Boylston",
+    "Arlington",
+    "Copley",
+    "Prudential",
+  ],
+  ["red", "blue", "orange", "grey"]
+);
+
+let greyLine = new TrainLines(
+  "grey",
+  ["Boylston", "Chinatown", "South Station"],
+  ["orange", "red", "green"]
+);
+
+// current state
+let currentLine = "grey";
+
+// lookup table for train lines
+let lineLookUp = {
+  grey: greyLine,
+  red: redLine,
+  blue: blueLine,
+  green: greenLine,
+  orange: orangeLine,
+};
+
+// function to move between states
+function junctionFunction(newLine) {
+  // receive the new line
+  // IF it is a valid transition
+  if (lineLookUp[currentLine].connectingLines.includes(newLine)) {
+    //update the current line to new line
+    currentLine = newLine;
+  }
+  // ELSE throw error
+  else {
+    console.log(`You cannot transition from ${currentLine} to ${newLine}`);
+  }
+}
+
+console.log(currentLine) // => grey
+junctionFunction("orange") // update current line to orange
+console.log(currentLine) // => orange
+junctionFunction("grey") // update current line to grey
+console.log(currentLine) // => grey
+junctionFunction("blue") // error 
+console.log(currentLine) // => grey
+
